f2e0f68c640a9de819ba2387819a90c1acd7ec8c
DamienHodsall <damien20work03@gmail.com>
2022-07-25 12:31:26 -0700

    reformat something.py

--- source/something.py
+++ source/something.py
@@ -1,10 +1,16 @@
 def collatzgen(number):
+
 	while number != 1:
+
 		number = 3 * number + 1
+
 		while not(number % 2):
+
 			number /= 2
+
 		yield int(number)
 
 def collatz(number):
+
 	return [number] + [num for num in collatzgen(number)]
 
