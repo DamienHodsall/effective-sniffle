dd2cecb62c278877cb9b0b30a830c02b9af19a7a
DamienHodsall <damien20work03@gmail.com>
2022-07-25 08:42:14 -0700

    fixed logic in something.py

--- source/something.py
+++ source/something.py
@@ -1,9 +1,9 @@
 def collatzgen(number):
-	while True:
-		number = number * 3 + 1
+	while number != 1:
+		number = 3 * number + 1
 		while not(number % 2):
-			number / 2
-		yield number
+			number /= 2
+		yield int(number)
 
 def collatz(number):
 	return [number] + [num for num in collatzgen(number)]
